48e23003f4d9f9ca03f0d1ee0a99cfd7dae80172
Stefan <stefan@neuland.io>
2022-02-27 22:20:36 +0100

    more theme adjustement

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,22 +15,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
-
-TYPOGRIFY = True
 
 STATIC_PATHS = []
 
